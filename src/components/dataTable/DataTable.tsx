20630df2f3f18dc810ed65fe62b5160979d02ed0
Bruno Castro <104720539+BrunoCastroM@users.noreply.github.com>
2023-08-30 10:10:54 -0300

    Using MUI DataGrid for users table

--- src/components/dataTable/DataTable.tsx
+++ src/components/dataTable/DataTable.tsx
@@ -1,74 +1,46 @@
-import {
-  DataGrid,
-  GridColDef,
-  GridValueGetterParams,
-  GridToolbar,
-} from "@mui/x-data-grid";
+import { DataGrid, GridColDef, GridToolbar } from "@mui/x-data-grid";
 import "./dataTable.scss";
+import { Link } from "react-router-dom";
 
-export default function DataTable() {
-  const columns: GridColDef[] = [
-    { field: "id", headerName: "ID", width: 90 },
-    {
-      field: "avatar",
-      headerName: "Avatar",
-      width: 100,
-      renderCell: (params) => {
-        return <img src={params.row.img || "noavatar.png"} alt="user" />;
-      },
-    },
-    {
-      field: "firstName",
-      headerName: "First name",
-      width: 150,
-      editable: true,
-    },
-    {
-      field: "lastName",
-      headerName: "Last name",
-      width: 150,
-      editable: true,
-    },
-    {
-      field: "age",
-      headerName: "Age",
-      type: "number",
-      width: 110,
-      editable: true,
-    },
-    {
-      field: "fullName",
-      headerName: "Full name",
-      description: "this column has a value getter and is not sortable",
-      sortable: false,
-      width: 160,
-      valueGetter: (params: GridValueGetterParams) =>
-        `${params.row.firstName || ""} ${params.row.lastName || ""}`,
-    },
-  ];
+type Props = {
+  columns: GridColDef[];
+  rows: object[];
+  slug: string;
+};
 
-  const rows = [
-    { id: 1, lastName: "Silva", firstName: "Julio", age: 35 },
-    { id: 2, lastName: "Fernandes", firstName: "Fernando", age: 42 },
-    { id: 3, lastName: "Castro", firstName: "Roberto", age: 35 },
-    { id: 4, lastName: "Castro", firstName: "Roberta", age: 35 },
-    { id: 5, lastName: "Ortega", firstName: "Silvana", age: 35 },
-    { id: 6, lastName: "Amoedo", firstName: "Rafael", age: 35 },
-    { id: 7, lastName: "Silveira", firstName: "Tarantino", age: 35 },
-    { id: 8, lastName: "Olveira", firstName: "José", age: 35 },
-    { id: 9, lastName: "Girao", firstName: "Rafaela", age: 35 },
-  ];
+export default function DataTable(props: Props) {
+  const handleDelete = (id: number) => {
+    console.log(id + "has been deleted");
+  };
+
+  const actionColumn: GridColDef = {
+    field: "action",
+    headerName: "Action",
+    width: 200,
+    renderCell: (params) => {
+      return (
+        <div className="action">
+          <Link to={`/${props.slug}/${params.row.id}`}>
+            <img src="/view.svg" alt="button view" />
+          </Link>
+          <div className="delete" onClick={() => handleDelete(params.row.id)}>
+            <img src="/delete.svg" alt="button delete" />
+          </div>
+        </div>
+      );
+    },
+  };
 
   return (
     <div className="data_table">
       <DataGrid
         className="data_grid"
-        rows={rows}
-        columns={columns}
+        rows={props.rows}
+        columns={[...props.columns, actionColumn]}
         initialState={{
           pagination: {
             paginationModel: {
-              pageSize: 5,
+              pageSize: 10,
             },
           },
         }}
--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,5 +1,55 @@
+import { GridColDef } from "@mui/x-data-grid";
 import DataTable from "../../components/dataTable/DataTable";
 import "./users.scss";
+import { userRows } from "../../data";
+
+const columns: GridColDef[] = [
+  { field: "id", headerName: "ID", width: 90 },
+  {
+    field: "avatar",
+    headerName: "Avatar",
+    width: 100,
+    renderCell: (params) => {
+      return <img src={params.row.img || "noavatar.png"} alt="user" />;
+    },
+  },
+  {
+    field: "firstName",
+    headerName: "First name",
+    type: "string",
+    width: 150,
+  },
+  {
+    field: "lastName",
+    headerName: "Last name",
+    type: "string",
+    width: 150,
+  },
+  {
+    field: "email",
+    headerName: "Email",
+    type: "string",
+    width: 200,
+  },
+  {
+    field: "phone",
+    headerName: "Phone",
+    type: "string",
+    width: 200,
+  },
+  {
+    field: "createdAt",
+    headerName: "Created At",
+    type: "string",
+    width: 120,
+  },
+  {
+    field: "verified",
+    headerName: "Verified",
+    type: "boolean",
+    width: 150,
+  },
+];
 
 export default function Users() {
   return (
@@ -8,7 +58,7 @@ export default function Users() {
         <h1>Users</h1>
         <button>Add new user</button>
       </div>
-      <DataTable />
+      <DataTable slug="users" columns={columns} rows={userRows} />
     </div>
   );
 }
